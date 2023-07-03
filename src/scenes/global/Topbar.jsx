9ff45ad901d220d4ccd878bcf91ac5b0911f4883
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-07-03 23:29:34 +0530

    Topbar updates

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { Box, IconButton, useTheme } from "@mui/material";
 import { useContext } from "react";
 import { ColorModeContext, tokens } from "../../theme";
@@ -9,7 +10,30 @@ import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchIcon from "@mui/icons-material/Search";
 
+
+
+
 const Topbar = () => {
-    return <div>Topbar</div>
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const colorMode = useContext(ColorModeContext);
+
+
+    return (
+        <Box display="flex" justifyContent="space-between" p={2}>
+            {/* SEARCH BAR */}
+            <Box
+                display="flex"
+                backgroundColor={colors.primary[400]}
+                borderRadius="3px"
+            >
+                <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+                <IconButton type="button" sx={{ p: 1 }}>
+                    <SearchIcon />
+                </IconButton>
+            </Box>
+            <IconButton></IconButton>
+        </Box>
+    );
 }
 export default Topbar;
