9d7c38cb54140473b098ebf825fc1c70dc483724
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-07-04 22:47:29 +0530

    mew file

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import { Typography, Box, useTheme } from "@mui/material";
+import { tokens } from "../theme";
+
+const Header = () => {
+    return <div></div>
+}
+export default Header;
--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -107,7 +107,7 @@ const Sidebar = () => {
                                     fontWeight="bold"
                                     sx={{ m: "10px 0 0 0" }}
                                 >
-                                    Ed Roh
+                                    LEO sd
                                 </Typography>
                                 <Typography variant="h5" color={colors.greenAccent[500]}>
                                     VP Fancy Admin
