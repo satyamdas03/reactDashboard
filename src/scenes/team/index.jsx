79cc91fe14616c0138320215419ebe9e8fd01062
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-07-05 18:39:10 +0530

    team's index.jsx configured

--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import { Box, Typography, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
@@ -37,25 +36,74 @@ const Team = () => {
             flex: 1,
         },
         {
-            field: "access",
+            field: "accessLevel",
             headerName: "Access Level",
             flex: 1,
-            renderCell: () => {
-                //continue from here
-            }
+            renderCell: ({ row: { access } }) => {
+                return (
+                    <Box
+                        width="60%"
+                        m="0 auto"
+                        p="5px"
+                        display="flex"
+                        justifyContent="center"
+                        backgroundColor={
+                            access === "admin"
+                                ? colors.greenAccent[600]
+                                : access === "manager"
+                                    ? colors.greenAccent[700]
+                                    : colors.greenAccent[700]
+                        }
+                        borderRadius="4px"
+                    >
+                        {access === "admin" && <AdminPanelSettingsOutlinedIcon />}
+                        {access === "manager" && <SecurityOutlinedIcon />}
+                        {access === "user" && <LockOpenOutlinedIcon />}
+                        <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
+                            {access}
+                        </Typography>
+                    </Box>
+                );
+            },
         },
     ];
+
     return (
-        <Box>
-            <Header title="TEAM" subtitle="Managing the team managers" />
-            <Box>
-                <DataGrid
-                    rows={mockDataTeam}
-                    columns={columns}
-                />
+        <Box m="20px">
+            <Header title="TEAM" subtitle="Managing the Team Members" />
+            <Box
+                m="40px 0 0 0"
+                height="75vh"
+                sx={{
+                    "& .MuiDataGrid-root": {
+                        border: "none",
+                    },
+                    "& .MuiDataGrid-cell": {
+                        borderBottom: "none",
+                    },
+                    "& .name-column--cell": {
+                        color: colors.greenAccent[300],
+                    },
+                    "& .MuiDataGrid-columnHeaders": {
+                        backgroundColor: colors.blueAccent[700],
+                        borderBottom: "none",
+                    },
+                    "& .MuiDataGrid-virtualScroller": {
+                        backgroundColor: colors.primary[400],
+                    },
+                    "& .MuiDataGrid-footerContainer": {
+                        borderTop: "none",
+                        backgroundColor: colors.blueAccent[700],
+                    },
+                    "& .MuiCheckbox-root": {
+                        color: `${colors.greenAccent[200]} !important`,
+                    },
+                }}
+            >
+                <DataGrid checkboxSelection rows={mockDataTeam} columns={columns} />
             </Box>
         </Box>
     );
-}
+};
 
 export default Team;
