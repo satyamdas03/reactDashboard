19e16db7825ab4b22e847536c9446ccc1c8cdee2
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-07-05 15:22:46 +0530

    Header.jsx started

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
+/* eslint-disable no-unused-vars */
 import { Typography, Box, useTheme } from "@mui/material";
 import { tokens } from "../theme";
 
-const Header = () => {
-    return <div></div>
+const Header = ({ title, subtitle }) => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    return (
+        <Box mb="30px">
+            <Typography variant="h2" color={colors.grey[100]} fontWeight="bold" sx={{ mb: "5px" }}>{title}</Typography>
+            <Typography variant="h5" color={colors.greenAccent[400]} >{subtitle}</Typography>
+        </Box >
+    );
 }
-export default Header;
+export default Header; 
