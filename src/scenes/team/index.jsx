001791e7d216de6c3eb693ec48af4ef53558c5f3
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-07-05 18:47:49 +0530

    contacts configured

--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -95,9 +95,9 @@ const Team = () => {
                         borderTop: "none",
                         backgroundColor: colors.blueAccent[700],
                     },
-                    "& .MuiCheckbox-root": {
-                        color: `${colors.greenAccent[200]} !important`,
-                    },
+                    // "& .MuiCheckbox-root": {
+                    //     color: `${colors.greenAccent[200]} !important`,
+                    // },
                 }}
             >
                 <DataGrid checkboxSelection rows={mockDataTeam} columns={columns} />
