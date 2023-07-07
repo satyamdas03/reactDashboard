4e3b822d1ee9b6eedd5d2f42c27dc4e445d67351
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-07-07 10:09:48 +0530

    form continue

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -5,11 +5,40 @@ import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
 
+const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    contact: "",
+    address1: "",
+    address2: "",
+};
+
+const userSchema = yup.object().shape({
+    firstName: yup.string().required("required"),
+    lastName: yup.string().required("required"),
+    email: yup.string().required("required"),
+    contact: yup.string().required("required"),
+    address1: yup.string().required("required"),
+    address2: yup.string().required("required"),
+
+});
+
 const Form = () => {
     const isNonMobile = useMediaQuery("(min-width:600px)");
+    const handleFormSubmit = (values) => {
+        console.log(values);
+    }
     return (
         <Box m="20px">
+            <Header title="CREATE USER" subtitle="Create a New User Profile" />
+            <Formik
+                onSubmit={handleFormSubmit}
+                initialValues={initialValues}
+                validationSchema={userSchema}
+            >
 
+            </Formik>
         </Box>
     );
 }
