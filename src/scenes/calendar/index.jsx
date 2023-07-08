7e7687a5500a48f688847d3f54eabbb0d82021ad
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-07-08 22:14:35 +0530

    calendar first config

---
+++ src/scenes/calendar/index.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import FullCalendar, { formatDate } from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+
