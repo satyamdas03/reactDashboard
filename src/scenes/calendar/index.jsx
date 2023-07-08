ff1b575050eed452c016910f80a6a4440fb859e5
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-07-09 00:17:15 +0530

    calendar final update

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -76,6 +76,43 @@ const Calendar = () => {
                     </List>
                 </Box>
                 {/* commit  */}
+                <Box flex="1 1 100%" ml="15px">
+                    <FullCalendar
+                        height="75vh"
+                        plugins={[
+                            dayGridPlugin,
+                            timeGridPlugin,
+                            interactionPlugin,
+                            listPlugin
+                        ]}
+                        headerToolbar={{
+                            left: "prev, next today",
+                            center: "title",
+                            right: "dayGridMonth, timeGridWeek, timeGridDay, listMonth"
+                        }}
+                        initialView="dayGridMonth"
+                        editable={true}
+                        selectable={true}
+                        selectMirror={true}
+                        dayMaxEvents={true}
+                        select={handleDateClick}
+                        eventClick={handleEventClick}
+                        eventsSet={(events) => setCurrentEvents(events)}
+                        initialEvents={[
+                            {
+                                id: "12315",
+                                title: "All-day event",
+                                date: "2022-09-14",
+                            },
+                            {
+                                id: "5123",
+                                title: "Timed event",
+                                date: "2022-09-28",
+                            },
+                        ]}
+
+                    />
+                </Box>
             </Box>
         </Box>
     );
