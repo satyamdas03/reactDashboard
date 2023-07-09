2249abe6a24f770fa2ecef5610159e173fa70064
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-07-09 10:21:11 +0530

    faq second commit

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -1,2 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { Box, useTheme } from "@mui/material";
+import Header from "../../components/Header";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
