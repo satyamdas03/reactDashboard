fbd887ea3479114e6abe6c6f33ab41f7dd1737ef
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-07-09 11:12:20 +0530

    faq third commit

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -5,3 +5,19 @@ import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
+import { tokens } from "../../theme";
+
+const FAQ = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    return (
+        <Box m="20px">
+            <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
+            <Accordion defaultExpanded>
+                <AccordionSummary>
+
+                </AccordionSummary>
+            </Accordion>
+        </Box>
+    );
+}
