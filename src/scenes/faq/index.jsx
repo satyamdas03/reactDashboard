92cc7cf1cd96f8ffd2926afa5063089b1151a15b
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-07-09 12:06:02 +0530

    faq fourth commit

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -6,6 +6,7 @@ import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import { tokens } from "../../theme";
+import { GridExpandMoreIcon } from "@mui/x-data-grid";
 
 const FAQ = () => {
     const theme = useTheme();
@@ -14,7 +15,20 @@ const FAQ = () => {
         <Box m="20px">
             <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
             <Accordion defaultExpanded>
-                <AccordionSummary>
+                <AccordionSummary expandIcon={<GridExpandMoreIcon />}>
+                    <Typography colors={colors.greenAccent[500]} variant="h5">
+                        An Important Questions
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion defaultExpanded>
+                <AccordionSummary expandIcon={<GridExpandMoreIcon />}>
 
                 </AccordionSummary>
             </Accordion>
