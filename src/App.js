4ccbb88c46c6adc9e2052a42b92279f462c868f9
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-07-10 19:04:41 +0530

    bar

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Form from "./scenes/form";
 // import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-import Calendar from "./scenes/calendar/calendar";
+import Calendar from "./scenes/calendar";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import Topbar from "./scenes/global/Topbar";
---
+++ src/scenes/bar/index.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+import BarChart from "../../components/BarChart";
+
+const Bar = () => {
+
+}
+export default Bar;
