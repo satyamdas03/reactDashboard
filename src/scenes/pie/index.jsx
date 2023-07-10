e4ed97667457d1519f987c40e554db327885764d
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-07-10 20:39:59 +0530

    ds

--- src/scenes/pie/index.jsx
+++ src/scenes/pie/index.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable no-unused-vars */
 import { Box } from "@mui/material";
 import Header from "../../components/Header";
-import BarChart from "../../components/BarChart";
+import PieChart from "../../components/PieChart";
 
 const Bar = () => {
     return (
         <Box m="20px">
-            <Header title="Bar Chart" subtitle="Simple Bar Chart" />
+            <Header title="Pie Chart" subtitle="Simple Bar Chart" />
             <Box height="75vh">
-                <BarChart />
+                <PieChart />
             </Box>
         </Box>
     );
