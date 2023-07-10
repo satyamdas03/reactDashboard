ca6a02795c3bfc4a0f325c2b761c52c1d4ff4a02
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-07-10 20:34:39 +0530

    pie

---
+++ src/components/PieChart.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import { ResponsivePie } from "@nivo/pie";
+import { tokens } from "../theme";
+import { useTheme } from "@mui/material";
+import { mockPieData as data } from "../data/mockData";
+
+const PieChart = () => {
+
+}
+export default PieChart;
