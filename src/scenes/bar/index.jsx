38f249f756858619ea527e4280a932f4841653ea
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-07-10 20:29:09 +0530

    bar

--- src/scenes/bar/index.jsx
+++ src/scenes/bar/index.jsx
@@ -4,6 +4,13 @@ import Header from "../../components/Header";
 import BarChart from "../../components/BarChart";
 
 const Bar = () => {
-
+    return (
+        <Box m="20px">
+            <Header title="Bar Chart" subtitle="Simple Bar Chart" />
+            <Box height="75vh">
+                <BarChart />
+            </Box>
+        </Box>
+    );
 }
 export default Bar;
