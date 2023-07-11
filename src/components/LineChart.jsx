1f8e1824d8c8f05b50b921f72a0e8c88b121de0c
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-07-11 12:19:24 +0530

    line chart first commit

---
+++ src/components/LineChart.jsx
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars */
+import { ResponsiveLine } from "@nivo/line";
+import { useTheme } from "@mui/material";
+import { tokens } from "../theme";
+import { mockLineData as data } from "../data/mockData";
