15291062ff01546a4fc2b0dab6f33280fdcf1f85
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-07-11 18:59:03 +0530

    geo first

---
+++ src/components/GeographyChart.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+import { useTheme } from "@mui/material";
+import { ResponsiveChoropleth } from "@nivo/geo";
+import { geoFeatures } from "../data/mockGeoFeatures";
+import { tokens } from "../theme";
+import { mockGeographyData as data } from "../data/mockData";
+
