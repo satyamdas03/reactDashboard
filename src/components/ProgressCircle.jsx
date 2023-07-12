556091f54f059b51ae86adb25ceb5776ef666b81
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-07-12 20:27:45 +0530

    progress second

--- src/components/ProgressCircle.jsx
+++ src/components/ProgressCircle.jsx
@@ -3,6 +3,8 @@ import { Box, useTheme } from "@mui/material";
 import { tokens } from "../theme";
 
 const ProgressCircle = ({ progress = "0.75", size = "40" }) => {
-
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const angle = progress * 360;
 }
 export default ProgressCircle;
