661c12043831c868bef756a919a460ded5d531a7
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-07-12 20:25:53 +0530

    progress

---
+++ src/components/ProgressCircle.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import { Box, useTheme } from "@mui/material";
+import { tokens } from "../theme";
+
+const ProgressCircle = ({ progress = "0.75", size = "40" }) => {
+
+}
+export default ProgressCircle;
